b5252367de978bc775a0e73504ff943ef06371c4
Yaroslav82 <yargay230607@gmail.com>
2023-09-27 13:09:28 +0300

    Create AppView

---
+++ src/main/java/org/example/app/views/AppView.java
@@ -0,0 +1,42 @@
+package org.example.app.views;
+
+import org.example.app.utils.AppStarter;
+import org.example.app.utils.Constants;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AppView {
+
+    private final Scanner scanner = new Scanner(System.in);;
+    private int option;
+
+    public int chooseOption() {
+        showMenu();
+        try {
+            option = scanner.nextInt();
+        } catch (InputMismatchException ime) {
+            System.out.println(Constants.INCORRECT_VALUE_MSG);
+            AppStarter.startApp();
+        }
+        return option;
+    }
+
+    private void showMenu() {
+        System.out.print("""
+                
+                ___________ MENU ___________
+                1 - Create user.
+                2 - View users.
+                3 - Update the user's phone.
+                4 - Delete user.
+                0 - Close the App.
+                """);
+    }
+
+    public void getOutput(int choice, String output) {
+        if (choice == 0) System.out.println(output);
+        scanner.close();
+        System.exit(0);
+    }
+}
